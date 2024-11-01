e4d3b5d72f65a888de1c075245b728a59df9e0c4
Gianx64 <GianPol1337@gmail.com>
2024-11-01 03:44:44 -0300

    Tailwind removal

---
+++ constants/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  colors: {
+    primary: "#161622",
+    secondary: {
+      DEFAULT: "#FF0000",
+      100: "#FF0000",
+      200: "#FF2200",
+    },
+    black: {
+      DEFAULT: "#000",
+      100: "#1E1E2D",
+      200: "#232533",
+    },
+    gray: {
+      100: "#CDCDE0",
+    },
+  },
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
